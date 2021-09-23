0fd09c41f2c9a3cad766265ccdd5ea765f9957a5
predrag.skokovic <pskokovic@qualityhouse.com>
2021-09-23 12:26:30 +0200

    updated EditProfileLib with logger instance

--- src/test/java/com/qualityhouse/course/ait/structured_scripting/library/EditProfileLib.java
+++ src/test/java/com/qualityhouse/course/ait/structured_scripting/library/EditProfileLib.java
@@ -1,7 +1,13 @@
 package com.qualityhouse.course.ait.structured_scripting.library;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class EditProfileLib {
+
+    private final static Logger logger = LogManager.getLogger(EditProfileLib.class);
+
     // todo: Exercise no. 3.a - create function for editing profile
 
 }
